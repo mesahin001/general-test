de85ed08ece268d4b6d7255ce0e2eeaf67725ceb
mesahin001 <62426828+mesahin001@users.noreply.github.com>
2023-03-07 16:39:36 +0100

    Create testif.sh

---
+++ testif.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+STR_1=$(docker exec -it shardeum-dashboard operator-cli status | awk '/state:/ {print $NF}')
+STR_2="stopped"
+echo $STR_1
+echo $STR_2
+if [ $STR_1 eq $STR_2 ]
+then
+        echo "Strings are equal"
+else
+        echo "Strings are not equal"
+fi
