709e59dec9ed6f616eda29bd44e20f6dc9805f39
mesahin001 <62426828+mesahin001@users.noreply.github.com>
2023-03-07 19:46:44 +0100

    Update sss.sh

--- sss.sh
+++ sss.sh
@@ -3,16 +3,17 @@ while true; do
   #run
   zaman=$(date '+%Y/%m/%d %H:%M:%S')  
   status=$(docker exec -it shardeum-dashboard operator-cli status | awk '/state:/ {print $NF}')
-   echo "Node in $status Coder Msahin & Hercules"
+   echo "Node status searching Coder Msahin & Hercules"
+   echo "Node status: $status"
    if [[ "$status" == *"top"* ]]
    then 
-  echo "Your node stopped and I am trying to start now $zaman"
+  echo "Your node stopped and I am trying to start now: $zaman"
   sleep 2
     # if 
   docker exec -it shardeum-dashboard operator-cli start 
   sleep 10 
   status=$(docker exec -it shardeum-dashboard operator-cli status | awk '/state:/ {print $NF}')
-  echo "Node in $status"
+  echo "Node status: $status"
   else
   sleep 1
   fi 
